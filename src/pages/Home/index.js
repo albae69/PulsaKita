8ddaf046875793b51318d93d95159055d866bda2
albae69 <ahmadalbaihaqi69@gmail.com>
2020-08-21 16:48:41 +0700

    added: hero section

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,25 @@
 import React from 'react'
 
+import img from '../../assets/images/mobile-pay.svg'
 import './style.scss'
 
 const Home = () => {
     return (
         <div className='jumbotron'>
             <div className='container'>
-                <h1 className='display-4'>Fluid jumbotron</h1>
-                <p className='lead'>
-                    This is a modified jumbotron that occupies the entire
-                    horizontal space of its parent.
-                </p>
+                <div className='row'>
+                    <div className='col-sm'>
+                        <img src={img} alt='' className='img' />
+                    </div>
+                    <div className='col-sm'>
+                        <h1 className='display-6'>Isi Ulang Pulsa Anda</h1>
+                        <p className='lead'>
+                            isi ulang pulsa gapake ribet di{' '}
+                            <span style={{ color: '#112557' }}>Pulsa</span>
+                            Kita tersedia juga Pembayaran Listrik, Dll.
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     )
